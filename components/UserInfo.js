32a70cc72cc0ac13e3f7473f76a099bfa787cce7
Sebastian Regules <serecruz97@hotmail.com>
2025-03-15 12:27:54 -0600

    crear clase UserInfo para ser usada en el perfil del usuario

--- components/UserInfo.js
+++ components/UserInfo.js
@@ -1,1 +1,21 @@
-export default class UserInfo {}
+export default class UserInfo {
+  constructor({ nameSelector, aboutSelector }) {
+    this._nameElement = document.querySelector(nameSelector);
+    this._aboutElement = document.querySelector(aboutSelector);
+  }
+  getUserInfo() {
+    return {
+      name: this._nameElement.textContent,
+      about: this._aboutElement.textContent,
+    };
+  }
+
+  setUserInfo({ name, about }) {
+    if (name) {
+      this._nameElement.textContent = name;
+    }
+    if (about) {
+      this._aboutElement.textContent = about;
+    }
+  }
+}
